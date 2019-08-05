6a8549295b7051b8e03cb8bb6ee2a203f10d9698
Raphael Krieger <rkriegersf@gmail.com>
2019-08-05 11:24:58 -0300

    - Logo: Add Symbol - Add sections text - Add new colors - Change JS, offset -40

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -3,6 +3,6 @@
 jQuery(function ($) {
     "use strict";
 
-    $("#menu").scrollspy({activeClass: "active", duration: 500, animate: true, offset: 43});
+    $("#menu").scrollspy({activeClass: "active", duration: 500, animate: true, offset: -40});
 
 }(jQuery));
